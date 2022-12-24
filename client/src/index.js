311f1da4f05419a28d981ed96d51b2ffe5d434c0
Avesh <aveshhasanfatta1155@gmail.com>
2022-12-24 21:26:08 +0530

    solved google signup error

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import store from "./store/index";
 import App from "./App.js";
 import React from "react";
 import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 // import store from "./store/ConfigStore/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <GoogleOAuthProvider clientId="276320922714-ls8q6jdh2rbfc8qe3ck81d9a8096q5mm.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </Provider>
 );
 
