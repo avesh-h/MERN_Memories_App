360fa3a0f49a572e673d45b454d8d840ff082d73
Avesh <aveshhasanfatta1155@gmail.com>
2023-01-16 21:02:17 +0530

    vercel server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import postRoutes from "./routes/postRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
 
 app.use(cors());
