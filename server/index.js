ca49eb35756a8808d970ce3e5687330fdca90099
Avesh <aveshhasanfatta1155@gmail.com>
2023-01-22 23:05:26 +0530

    added middleware

--- server/index.js
+++ server/index.js
@@ -16,7 +16,20 @@ const app = express();
 // };
 
 //for live
-app.use(cors(allowCors));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  next();
+});
+app.use(cors());
 
 //for local
 // app.use(cors());
