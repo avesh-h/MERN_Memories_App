266ddadea53eb14f41532581a9d5bbce9665d090
Avesh <aveshhasanfatta1155@gmail.com>
2023-01-22 23:33:17 +0530

    add webname in origin

--- server/index.js
+++ server/index.js
@@ -19,7 +19,10 @@ const app = express();
 app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://mern-memories-app-xi.vercel.app"
+  );
   res.header(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
