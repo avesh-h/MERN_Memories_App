eeed2be44f93f63880bf4d18cc97eb7a74e40476
Avesh <aveshhasanfatta1155@gmail.com>
2023-01-22 23:37:23 +0530

    change url

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Origin",
-    "https://mern-memories-app-xi.vercel.app"
+    "https://mern-memories-app-9dwwg8pja-avesh-h.vercel.app/"
   );
   res.header(
     "Access-Control-Allow-Methods",
