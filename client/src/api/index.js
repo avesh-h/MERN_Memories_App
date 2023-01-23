e7c9c68923435a27a3139c51dfbc77a9f7447af6
Avesh <aveshhasanfatta1155@gmail.com>
2023-01-23 23:47:10 +0530

    previous push

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 //for Live project
 const API = axios.create({
-  baseURL: "https://mern-memories-app-xi.vercel.app",
+  baseURL: "https://mern-memories-app-9dwwg8pja-avesh-h.vercel.app",
+  // baseURL: "https://mern-memories-app-xi.vercel.app",
 });
 
 //For production
