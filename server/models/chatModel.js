5bd5ff5e46f52786bcc8911e2c2696802f126b9d
Avesh <aveshhasanfatta1155@gmail.com>
2023-03-14 18:55:02 +0530

    add schema for chat feature

---
+++ server/models/chatModel.js
@@ -0,0 +1,27 @@
+//These field are going to added inside the schema
+
+//chatName
+//isGroupChat
+//users
+//latestMessage
+//groupAdmin
+
+//ref:"User"
+//It means reference like which model is reference to like in this case it's reference to the usersModel
+
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timeStamps: true }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.exports = Chat;
