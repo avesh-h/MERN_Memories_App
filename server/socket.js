d85f324aaad19c71c265be0e67ab7dd31f84322d
Avesh <aveshhasanfatta1155@gmail.com>
2024-09-22 22:17:22 +0530

    fix : socket url issue in live

--- server/socket.js
+++ server/socket.js
@@ -8,7 +8,7 @@ export let io;
 export const init = (server) => {
   io = new Server(server, {
     pingTimeout: 60000, //it will wait for the 1 min to for check any response from the client if there is nothing then it closed in 1 min to save the bandwidth.
-    cors: { origin: process.env.LIVE_URL },
+    cors: { origin: process.env.FRONTEND_URL },
     // Testing with mobile
     // cors: { origin: "http://192.168.1.17:3000" },
   });
